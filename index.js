4c50a38a3bbc586b213e5c631836548b6569ec9a
ALEJANDRO <jalejandrob95@gmail.com>
2022-06-04 02:02:19 -0500

    [IMP] Cors allow origins

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routersApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./midlewares/error.handler');
 
@@ -7,6 +8,18 @@ const port = 3003;
 
 //Midlewares
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://localhost:8081'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+app.use(cors(corsOptions));
 
 //Routing
 routersApi(app);
